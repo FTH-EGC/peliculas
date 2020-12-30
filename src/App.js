8efbc7452ab400b0d6eeb520e37cd6ec80e5f425
e.g.c <ironmaiden-erick@hotmail.com>
2020-12-29 19:36:34 -0600

    Creando el Formulario

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import Header from './components/Header';
+import Formulario from './components/Formulario'
 
 
 function App() {
   return (
+    <Fragment>
+
+
     <Header />
+    <div className="contenedor">
+      <div className="contenido">
+        <Formulario />
+      </div>
+    </div>
+
+    </Fragment>
+
   );
 }
 
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -4,7 +4,33 @@ import React from 'react';
 const Formulario = () => {
     return ( 
 
-        
+        <form action=""
+        className="formulario"
+        >
+        <label htmlFor="titulo">Titulo de la pelicula</label>
+        <input
+            type="text"
+            name="titulo"
+
+        />
+        <label htmlFor="year">Año:</label>
+        <input
+            type="text"
+            name="year"
+        />
+        <label htmlFor="trama">Trama:</label>
+        <select 
+        name="trama">
+            <option value="!#">--Seleccione--</option>
+            <option value="short">Pequeña</option>
+            <option value="full">Completa</option>
+        </select>
+
+        <input 
+            type="submit"
+            value="Buscar"
+        />
+        </form>
 
      );
 }
